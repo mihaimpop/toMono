cd8a2f0532030a772d00e4479c5b92067fe236d4
Mihai <mihai.pop2@fanduel.com>
2024-07-04 21:06:21 +0300

    Changed to manifest v3

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 chrome.webNavigation.onDOMContentLoaded.addListener(() => {
-  chrome.browserAction.setIcon({ path: "/Icon-128-disabled.png" });
+  chrome.action.setIcon({ path: "/Icon-128-disabled.png" });
 });
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.executeScript(null, { file: "content.js" });
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"]
+  });
 });
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === 'updateIcon') {
     if (msg.value) {
-      chrome.browserAction.setIcon({ path: "/Icon-128-enabled.png" });
+      chrome.action.setIcon({ path: "/Icon-128-enabled.png" });
     } else if (msg.value === false) {
-      chrome.browserAction.setIcon({ path: "/Icon-128-disabled.png" });
+      chrome.action.setIcon({ path: "/Icon-128-disabled.png" });
     }
   }
 });
